13310dde0f52c5df4dbcdabef184486089daf179
Sambert Rodrigues <sambert@smartshore.nl>
2023-03-01 15:54:24 +0530

    290

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,70 @@
     <h2>{{ userName }}</h2>
     <h3>{{ age }}</h3>
     <button @click="setAge">ChangeAge</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
-import { ref,reactive ,toRefs} from 'vue';//isReactive , isRef
+import { ref, computed } from 'vue'; //isReactive , isRef
 
 export default {
-
   setup() {
     const uAge = ref(31);
-    const user = reactive({ //reactive only works with objects
-      name:'Sambert',
-      age: 99
+    const firstName = ref('asdsad');
+    const lastName = ref('sadds');
+    // const user = reactive({
+    //   //reactive only works with objects
+    //   name: 'Sambert',
+    //   age: 99,
+    // });
+
+
+    const uName = computed(function(){
+      return firstName.value + ' ' + lastName.value;
     })
 
-    function setNewAge() {
-      user.age = 55;
+    // function setNewAge() {
+    //   user.age = 55;
+    // }
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+    function setLastName(event) {
+      lastName.value = event.target.value;
     }
 
     // console.log(isReactive(user));
     // console.log(isReactive(uAge.value));
     console.log(uAge);
 
+    // setTimeout(function () {
+    //   // uName.value = 'Sam',
+    //   // uAge.value = 33
 
-    setTimeout(function(){
-      // uName.value = 'Sam',
-      // uAge.value = 33
+    //   // ** with refs
+    //   // user.value.name = "Sam";
+    //   // user.value.age = 31;
 
-      // ** with refs
-      // user.value.name = "Sam";
-      // user.value.age = 31;
+    //   //**with reactive
+    //   user.name = 'Sam';
+    //   user.age = 31;
+    // }, 2000);
 
-      //**with reactive
-      user.name = "Sam";
-      user.age = 31;
-    }, 2000);
+    // const userRefs = toRefs(user);
 
-    setTimeout(function(){
-      user.age += 31;
-    }, 4000);
-      
-    const userRefs = toRefs(user);
-
-
-
-    return { user:user, userName: userRefs.name, age: userRefs.age , setAge: setNewAge}
-  }
+    return {
+      // user: user,
+      userName: uName, //computed
+      age: uAge, //change toRef
+      // setAge: setNewAge, //button
+      setFirstName,
+      setLastName,
+    };
+  },
   // data() {
   //   return {
   //     userName: 'Maximilian',
